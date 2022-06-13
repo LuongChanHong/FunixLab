f6c8ba79fa87b878332fe8ed6f482517ecf53b8b
honglc <hongmainmail@gmail.com>
2022-06-13 15:47:34 +0700

    rendered all staff detail info

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -35,7 +35,7 @@ class StaffList extends Component {
               <CardText>
                 Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
               </CardText>
-              <CardText>Phòng ban: </CardText>
+              <CardText>Phòng ban: {staff.department.name}</CardText>
               <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
               <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
             </CardBody>
