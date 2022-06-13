07ae00bb3550494d81fc846b692e0d4fc74efa78
honglc <hongmainmail@gmail.com>
2022-06-13 15:38:25 +0700

    rendered staff detail when clicked staff item

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -1,7 +1,9 @@
 import "../style/style.css";
 import React, { Component } from "react";
+import dateFormat from "dateformat";
+import { Card, CardTitle, CardBody, CardText } from "reactstrap";
 
-import Staff from "./Staff";
+// import StaffDetail from "./StaffDetail";
 
 class StaffList extends Component {
   constructor(props) {
@@ -9,19 +11,55 @@ class StaffList extends Component {
     this.state = {
       staffList: this.props.staffList,
       roleList: this.props.roleList,
-      departmentList: this.props.departmentList,
+      department: this.props.departmentList,
+      selectedStaff: null,
     };
+  }
+
+  selectStaff(staff) {
+    this.setState({ selectedStaff: staff });
+  }
+
+  renderStaffDetail(staff) {
+    if (staff != null) {
+      return (
+        <div className="col-12 my-3">
+          <Card>
+            <h5>{staff.name}</h5>
+          </Card>
+        </div>
+      );
+    } else {
+      <></>;
+    }
   }
 
   renderStaff() {
     let staffListHTML = this.state.staffList.map((staff) => {
-      return <Staff staffObject={staff} />;
+      return (
+        <div key={staff.id} className="col-lg-4 col-md-6 col-sm-12 my-1">
+          <Card
+            onClick={() => {
+              this.selectStaff(staff);
+            }}
+          >
+            {staff.name}
+          </Card>
+        </div>
+      );
     });
     return staffListHTML;
   }
 
   render() {
-    return <section className="row ">{this.renderStaff()}</section>;
+    return (
+      <section>
+        <div className="row">{this.renderStaff()}</div>
+        <div className="row">
+          {this.renderStaffDetail(this.state.selectedStaff)}
+        </div>
+      </section>
+    );
   }
 }
 
