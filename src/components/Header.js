6d48d7e8f98a6e5606523baece97d52b3781745d
honglc <hongmainmail@gmail.com>
2022-06-25 17:38:11 +0700

    LAB06_3 done

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,14 @@ import {
   NavItem,
   Collapse,
   NavbarToggler,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
@@ -14,17 +22,39 @@ class Header extends Component {
     super(props);
     this.state = {
       isNavbarOpen: false,
+      isModalOpen: false,
     };
     // Gán hàm toggleNavbar cho class Header để có thế sư dụng
     // cách khác là dùng arrow function
     // trỏ tới hàm toggleNavbar trong onClich thẻ cần dùng hàm này
     this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   toggleNavbar() {
     this.setState({
       isNavbarOpen: !this.state.isNavbarOpen,
     });
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+    alert(
+      "username:" +
+        this.userName.value +
+        " password:" +
+        this.passWord.value +
+        " remember:" +
+        this.remember.checked
+    );
+    event.preventDefault();
   }
 
   render() {
@@ -70,9 +100,62 @@ class Header extends Component {
                   </NavLink>
                 </NavItem>
               </Nav>
+              {/* Login modal button */}
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"></span>login
+                  </Button>
+                </NavItem>
+              </Nav>
             </Collapse>
           </div>
         </Navbar>
+        <Modal isOpen={this.state.isModalOpen}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          {/* UNCONTROLED FORM */}
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="userName">Username:</Label>
+                <Input
+                  type="text"
+                  id="userName"
+                  name="userName"
+                  innerRef={(input) => {
+                    this.userName = input;
+                  }} // uncontrolled spot light
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="passWord">Password:</Label>
+                <Input
+                  type="password"
+                  id="passWord"
+                  name="passWord"
+                  innerRef={(input) => {
+                    this.passWord = input;
+                  }} // uncontrolled spot light
+                />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    name="remember"
+                    innerRef={(input) => {
+                      this.remember = input;
+                    }} // uncontrolled spot light
+                  />{" "}
+                  Remeber me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">
+                Login
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
       </React.Fragment>
     );
   }
