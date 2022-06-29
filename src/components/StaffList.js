4405b205672ca3084b98d3f2ac59f4347d8c8e68
honglc <hongmainmail@gmail.com>
2022-06-29 16:54:42 +0700

    add search staff by name function  in StaffList component

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -1,5 +1,16 @@
+//RJS101x_asm3_honglcfx16049
 import React, { Component } from "react";
-import { Card, CardBody, CardImg, CardText } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardImg,
+  CardText,
+  Form,
+  FormGroup,
+  Label,
+  Button,
+  Input,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
 import Header from "./Header";
@@ -8,11 +19,12 @@ import Footer from "./Footer";
 class StaffList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      staffList: this.props.staffList,
+    };
   }
 
-  renderStaffList = () => {
-    const list = this.props.staffList;
+  renderStaffList = (list) => {
     return list.map((staff) => (
       <div key={staff.id} className="col-sm-6 col-md-4 col-lg-2 my-1">
         <Card>
@@ -27,15 +39,60 @@ class StaffList extends Component {
     ));
   };
 
+  handleStaffListNull = () => {
+    return <h4 className="text-center text-danger">Không tìm thấy</h4>;
+  };
+
+  // Tìm theo tên nhân viên
+  searchByName = (event) => {
+    const searchInput = this.searchInput.value;
+    let list = [];
+    this.props.staffList.forEach((staff) => {
+      if (staff.name.toLowerCase().includes(searchInput.toLowerCase())) {
+        list.push(staff);
+      }
+    });
+    this.setState({ staffList: list });
+    event.preventDefault();
+  };
+
   render() {
     // console.log(this.props.staffList);
     return (
       <section className="component_bg">
         <Header />
         <div className="container">
-          <h1>Nhân viên</h1>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-center">
+              <h1>Nhân viên</h1>
+              <div className="m-2">
+                <Button onClick={() => alert("click")} color="success">
+                  ➕
+                </Button>
+              </div>
+            </div>
+            <div>
+              <Form onSubmit={this.searchByName} className="d-flex">
+                <Input
+                  type="text"
+                  id="searchInput"
+                  name="searchInput"
+                  innerRef={(inputValue) => (this.searchInput = inputValue)}
+                />
+                <Button type="submit" value="" color="success" className="">
+                  🔍
+                </Button>
+              </Form>
+            </div>
+          </div>
+
           <hr />
-          <div className="row">{this.renderStaffList()}</div>
+
+          <div className="row">
+            {this.state.staffList.length == 0
+              ? this.handleStaffListNull()
+              : this.renderStaffList(this.state.staffList)}
+          </div>
         </div>
         <Footer />
       </section>
