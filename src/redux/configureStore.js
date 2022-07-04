11a12237d6b3a2eb511f02b17423965924c76c15
honglc <hongmainmail@gmail.com>
2022-07-04 15:50:58 +0700

    LAB08_1 Combining Reducers done

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,17 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from "./reducer";
+import { createStore, combineReducers } from "redux";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Leaders } from "./leaders";
+import { Promotions } from "./promotions";
 
 export const ConfigureStore = () => {
-  const store = createStore(Reducer, initialState);
+  const store = createStore(
+    combineReducers({
+      dishList: Dishes,
+      commentList: Comments,
+      leaderList: Leaders,
+      promotionList: Promotions,
+    })
+  );
   return store;
 };
